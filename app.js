9b047abbf2e8d458140f90e2bd8aa9e1811a2ea8
Artyom Sommer <litovskys@gmail.com>
2022-06-04 00:20:16 +0300

    Finally, working timer.

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const birthdayInputForm = document.querySelector('#birthday-form');
 const birthdayInput = document.querySelector('#birthday-input');
-const birthdayTime = document.querySelector('#birthday-timer');
+const birthdayTimeContainer = document.querySelector('#birthday-timer-container');
+const birthdayTimer = document.querySelector('#birthday-timer');
+const birthdayTimerSeconds = document.querySelector('#birthday-timer-seconds');
+
+const UNIX_TIME_FULL_YEAR = new Date(0).getFullYear();
+const AGE_DELIMITER = 1000 * 60 * 60 * 24 * 365.25;
+const MAX_VALUE_LENGTH = 10;
+const UPDATE_TICK = 150;
+
+let birthday;
+let difference;
+let value;
 
 const user = {
    get birthday() {
@@ -18,12 +29,15 @@ const user = {
  * @param {Date} value Date value.
  */
 function isValidDate(value) {
-   return Boolean(
+   const isDate = Boolean(
       value instanceof Date &&
       value.getDay() &&
       value.getMonth() &&
       value.getFullYear()
-   );
+   )
+   const isLessDate = Date.now() - value > 0;
+
+   return isDate && isLessDate;
 }
 
 /**
@@ -44,11 +58,29 @@ function onSubmitBirthday(event) {
  * Sets active control for show.
  */
 function setActiveControl() {
-   const controlToHide = (user.birthday) ? birthdayInputForm : birthdayTime;
-   const controlToShow = (user.birthday) ? birthdayTime : birthdayInputForm;
+   const controlToHide = (user.birthday) ? birthdayInputForm : birthdayTimeContainer;
+   const controlToShow = (user.birthday) ? birthdayTimeContainer : birthdayInputForm;
 
    controlToHide.style.display = 'none';
    controlToShow.style.display = 'block';
+
+   if (user.birthday) {
+      birthday = new Date(user.birthday);
+
+      // Need to render start.
+      calculateAge();
+      setInterval(calculateAge, UPDATE_TICK);
+   }
+}
+
+/**
+ * Sets current age by interval.
+ */
+function calculateAge() {
+   difference = Date.now() - birthday;
+   value = (new Date(difference) - UNIX_TIME_FULL_YEAR) / AGE_DELIMITER;
+   birthdayTimer.innerText = Math.trunc(value);
+   birthdayTimerSeconds.innerText = (value % 1).toFixed(MAX_VALUE_LENGTH).substring(1);
 }
 
 /**
